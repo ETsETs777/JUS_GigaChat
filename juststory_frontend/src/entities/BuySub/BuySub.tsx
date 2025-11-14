23cadaef3d3c48d5d3b189e55323e54172bdc37b
ETsETs777 <cymbalzena087@gmail.com>
2025-11-14 14:52:09 +0300

    feat(frontend): implement Profile and BuySub components

---
+++ juststory_frontend/src/entities/BuySub/BuySub.tsx
@@ -0,0 +1,116 @@
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios"; // Импортируем Axios
+import styles from "./BuySub.module.css"; // Импортируем стили
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import { backendApiUrl } from "@/src/utils/backendApiUrl";
+import Header from "../Header/Header";
+
+interface Subscription {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+const BuySub: React.FC = () => {
+  const [subscriptions, setSubscriptions] = useState<Subscription[]>([]);
+  const [selectedSubscriptionId, setSelectedSubscriptionId] = useState<
+    number | null
+  >(null);
+  const [loading, setLoading] = useState<boolean>(true); // Состояние загрузки
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchSubscriptions = async () => {
+      try {
+        const response = await axios.get<Subscription[]>(
+          `${backendApiUrl}/subscription`
+        );
+        setSubscriptions(response.data);
+      } catch (error) {
+        console.error("Ошибка при получении подписок:", error);
+      } finally {
+        setLoading(false); // Устанавливаем состояние загрузки в false после завершения
+      }
+    };
+    fetchSubscriptions();
+  }, []);
+
+  const handleCheckboxChange = (id: number) => {
+    setSelectedSubscriptionId(id);
+  };
+
+  const handlePurchase = async () => {
+    const token = Cookies.get("token");
+    if (!token || selectedSubscriptionId === null) {
+      return alert("Пожалуйста, выберите подписку и войдите в систему.");
+    }
+    try {
+      const response = await axios.post(
+        `${backendApiUrl}/subscription/purchase/${selectedSubscriptionId}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(response);
+      if (response.status === 201) {
+        alert("Подписка успешно приобретена!");
+        router.push("/profile"); // Перенаправление на страницу профиля
+      } else {
+        alert("Ошибка при покупке подписки: " + response.data.message);
+      }
+    } catch (error: any) {
+      // Указываем тип error как any
+      alert(
+        "Ошибка при покупке подписки: " +
+          (error.response?.data?.message || error.message)
+      );
+    }
+  };
+
+  if (loading) {
+    return (
+      <div className={styles.loadingContainer}>
+        <div className={styles.loader}></div>
+        <p>Загрузка подписок...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Header />
+      <div className={styles.container}>
+        <h1 className={styles.header}>Выберите подписку</h1>
+        <div className={styles.subscriptionList}>
+          {subscriptions.map((subscription) => (
+            <div key={subscription.id} className={styles.subscriptionItem}>
+              <input
+                type="checkbox"
+                id={`subscription-${subscription.id}`}
+                name="subscription"
+                value={subscription.id}
+                onChange={() => handleCheckboxChange(subscription.id)}
+              />
+              <label htmlFor={`subscription-${subscription.id}`}>
+                <h2>{subscription.name}</h2>
+                <p>Цена: {subscription.price} руб.</p>
+                <p>Описание: {subscription.description}</p>
+              </label>
+            </div>
+          ))}
+        </div>
+        <button className={styles.purchaseButton} onClick={handlePurchase}>
+          Купить подписку
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BuySub;
---
+++ juststory_frontend/src/entities/Profile/Profile.tsx
@@ -0,0 +1,207 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserProfile } from "../../../lib/entities/user/userSlice";
+import { RootState } from "../../../lib/store";
+import { updateSubscription } from "../../../lib/entities/subscription/subscritpionSlice";
+import Cookies from "js-cookie";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import styles from "./Profile.module.css";
+import { validateToken } from "../../utils/validateToken";
+const Profile = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { user, loading, error } = useSelector(
+    (state: RootState) => state.user
+  );
+
+  const [subscriptionData, setSubscriptionData] = useState({
+    id: 1,
+    name: "",
+    price: "",
+    daysPeriod: 30,
+    description: "",
+  });
+
+  // Новые состояния для отслеживания успешности обновления
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [updateError, setUpdateError] = useState(false);
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      validateToken(token).then((valid) => {
+        if (!valid) {
+          router.push("/login");
+        }
+      });
+    } else {
+      router.push("/login");
+    }
+  }, [router]);
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (!token) {
+      router.push("/login");
+    } else {
+      dispatch(getUserProfile());
+    }
+  }, [dispatch, router]);
+
+  const handleLogout = () => {
+    Cookies.remove("token");
+    router.push("/login");
+  };
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setSubscriptionData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleUpdateSubscription = async () => {
+    if (user && user.role === "Admin") {
+      try {
+        await dispatch(
+          updateSubscription({
+            id: subscriptionData.id,
+            data: subscriptionData,
+          })
+        ).unwrap(); // Используем unwrap для обработки успешного выполнения
+        setUpdateSuccess(true);
+        setUpdateError(false);
+      } catch (err) {
+        setUpdateSuccess(false);
+        setUpdateError(true);
+      }
+    }
+  };
+
+  if (loading) {
+    return <p>Загрузка...</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <Header />
+      <div className={styles.profileContent}>
+        <h1 className={styles.header}>Профиль</h1>
+        {user && (
+          <div className={styles.profileInfo}>
+            <p>Имя: {user.name}</p>
+            <p>Подписка: {user.subscription ? "Имеется" : "Отсутствует"}</p>
+            <p>
+              Дата приобретения:{" "}
+              {user.subBuyTime
+                ? new Date(user.subBuyTime).toLocaleDateString()
+                : "Не указана"}
+            </p>
+            <p>
+              Дата окончания:{" "}
+              {user.subEndTime
+                ? new Date(user.subEndTime).toLocaleDateString()
+                : "Не указана"}
+            </p>
+            {user.role === "Admin" && <p>Роль: Администратор</p>}
+            {user.role === "Admin" && (
+              <div>
+                <h2>Обновление подписки</h2>
+                <input
+                  type="number"
+                  name="id"
+                  value={subscriptionData.id}
+                  onChange={handleInputChange}
+                  placeholder="Айди подписки"
+                  className={`${styles.input} ${
+                    updateSuccess
+                      ? styles.success
+                      : updateError
+                      ? styles.error
+                      : ""
+                  }`}
+                />
+                <input
+                  type="text"
+                  name="name"
+                  value={subscriptionData.name}
+                  onChange={handleInputChange}
+                  placeholder="Название подписки"
+                  className={`${styles.input} ${
+                    updateSuccess
+                      ? styles.success
+                      : updateError
+                      ? styles.error
+                      : ""
+                  }`}
+                />
+                <input
+                  type="text"
+                  name="price"
+                  value={subscriptionData.price}
+                  onChange={handleInputChange}
+                  placeholder="Цена подписки"
+                  className={`${styles.input} ${
+                    updateSuccess
+                      ? styles.success
+                      : updateError
+                      ? styles.error
+                      : ""
+                  }`}
+                />
+                <input
+                  type="number"
+                  name="daysPeriod"
+                  value={subscriptionData.daysPeriod}
+                  onChange={handleInputChange}
+                  placeholder="Период (дни)"
+                  className={`${styles.input} ${
+                    updateSuccess
+                      ? styles.success
+                      : updateError
+                      ? styles.error
+                      : ""
+                  }`}
+                />
+                <textarea
+                  name="description"
+                  value={subscriptionData.description}
+                  onChange={handleInputChange}
+                  placeholder="Описание подписки"
+                  className={`${styles.textarea} ${
+                    updateSuccess
+                      ? styles.success
+                      : updateError
+                      ? styles.error
+                      : ""
+                  }`}
+                />
+                <button
+                  onClick={handleUpdateSubscription}
+                  className={styles.updateButton}
+                >
+                  Обновить подписку
+                </button>
+              </div>
+            )}
+            <button className={styles.logoutButton} onClick={handleLogout}>
+              Выйти
+            </button>
+          </div>
+        )}
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Profile;
